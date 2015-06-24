7c700308ce1ce66cd57c96f327cbc80c55fa10a9
manuelcoppotelli <manuelcop@gmail.com>
2015-06-24 14:19:47 +0200

    Lesson 18
    Gravatars and View Presenters

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -19,15 +19,17 @@
             <ul class="nav navbar-nav navbar-right">
                 @if($currentUser)
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ $currentUser->name }} <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <img class="nav-gravatar" src="{{ $currentUser->present()->gravatar() }}" alt="{{ $currentUser->name }}"/>
+
+                            {{ $currentUser->name }} <span class="caret"></span>
+                        </a>
                         <ul class="dropdown-menu">
                             <li><a href="#">Action</a></li>
                             <li><a href="#">Another action</a></li>
                             <li><a href="#">Something else here</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">One more separated link</a></li>
+                            <li>{!! link_to_route('logout_path', 'Log Out') !!}</li>
                         </ul>
                     </li>
                 @else
